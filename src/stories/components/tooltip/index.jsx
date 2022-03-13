96a33d313e0cf30dfbd498920065c1172b3a5820
oluwatobiloba2000 <ananioluwatobiloba2000@gmail.com>
2022-03-13 14:22:32 +0100

    clean up code and add markdown docs

--- src/stories/components/tooltip/index.jsx
+++ src/stories/components/tooltip/index.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-/**
- * Primary UI component for user interaction
- */
 export const Tooltip = ({ color, backgroundColor, position, text, children }) => {
-//   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <div
       className={'ui-tooltip--trigger'}
---
+++ src/stories/components/tooltip/tooltip.md
@@ -0,0 +1,4 @@
+# Tooltip component
+
+## Preview
+https://ui-labs.netlify.app/?path=/story/component-tooltip--top
