7008f3b457a394e34ac488ccd9b529af802ecd2b
oluwatobiloba2000 <ananioluwatobiloba2000@gmail.com>
2022-03-13 14:55:40 +0100

    clean up code

--- src/stories/components/tooltip/Tooltip.stories.jsx
+++ src/stories/components/tooltip/Tooltip.stories.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Button } from '../button/Button';
-
 import { Tooltip } from './';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Component/Tooltip',
   component: Tooltip,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
 const Template = (args) => {
   return <div style={{margin: '100px'}}>
         <Tooltip {...args}>
@@ -23,7 +21,7 @@ const Template = (args) => {
 };
 
 export const Top = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Top.args = {
   backgroundColor: 'black',
   position: 'top',
